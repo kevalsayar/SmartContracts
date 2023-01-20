ec526d8800a0f3a57d123cd67fe8386b4794d06a
Keval <keval.sayar@globalvoxinc.com>
2023-01-20 12:26:52 +0530

    changes to BEP-20 token

--- BEP-20 Token/migrations/1_deploy_contracts.js
+++ BEP-20 Token/migrations/1_deploy_contracts.js
@@ -1,28 +1,7 @@
 require("dotenv").config();
 var SampleToken = artifacts.require("SampleToken");
-// var TokenVesting = artifacts.require("TokenVesting");
 const { name, symbol, supply } = process.env;
-// var TokenVestingV1 = artifacts.require("TokenVestingV1");
-// const { deployProxy, upgradeProxy } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer) {
   await deployer.deploy(SampleToken, name, symbol, supply);
-  // const BEP20TokenInstance = await BEP20Token.deployed();
-  // await deployer.deploy(
-  //   TokenVesting,
-  //   "0x026Af2C078fdFc24e6F4678D50350903CE244F7E",
-  //   1674131400,
-  //   60,
-  //   720,
-  //   60
-  // );
-
-  // await deployProxy(TokenVesting, [BEP20TokenInstance.address], {
-  //   deployer,
-  //   initializer: "initialize",
-  // });
-  // const instance = await TokenVesting.deployed();
-  // const upgrade = await upgradeProxy(instance.address, TokenVestingV1, {
-  //   deployer,
-  // });
 };
